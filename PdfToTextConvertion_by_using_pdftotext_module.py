04580d742005e599cbfc43ca27982d448da32bc6
mrfatihasci <126919157+mrfatihasci@users.noreply.github.com>
2023-10-27 16:54:08 +0300

    PdfToTextConvertion_by_using_pdftotext_module.py

---
+++ PdfToTextConvertion_by_using_pdftotext_module.py
@@ -0,0 +1,4 @@
+#python code
+
+pdftotext -layout name.pdf name2.txt           (write it in terminal to convert pdf to txt by using pdftotext module, create the empty txt file beforehand, and make sure both pdf and txt file are in the same file)
+
